811260eea50ef3e335017a8d0ec691f266660ea5
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-01-27 18:49:27 +0100

    username

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -7,11 +7,11 @@ const AddUser = props => {
 
     return (
         <form onSubmit={AddUserHandler}>
-            <label htmlFor="username">Add user</label>
+            <label htmlFor="username">Username</label>
             <input id="username" type="text"/>
-            <label htmlFor="age">Age (years)</label>
+            <label htmlFor="age">Age (Years)</label>
             <input id="age" type="number"/>
-            <button type="submit">User</button>
+            <button type="submit">Add User</button>
         </form>
     )
 };
