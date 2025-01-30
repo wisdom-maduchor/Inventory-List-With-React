0a1ca5c2363b3c123fb3f197559300257052cc69
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-01-30 23:52:30 +0100

    userList con't

--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from './UsersList.module.css'
 
 const UsersList = (props) => {
     return (
-        <ul>
-            {props.users.map((user) => (
-                <li>
-                    {user.name} ({user.age} years old)
-                </li>
-            ))}
-        </ul>
+        <Card className={classes.users}>
+            <ul>
+                {props.users.map((user) => (
+                    <li>
+                        {user.name} ({user.age} years old)
+                    </li>
+                ))}
+            </ul>
+        </Card>
     )
 }
 
