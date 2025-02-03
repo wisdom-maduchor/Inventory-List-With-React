7dff19359c25ccf228be6987abd6782f581232bc
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-02-03 13:15:57 +0100

    creation of ERROR MODAL component, how to make it a modal and fade the background content so user is not able to interact with the its information when modal is active

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 // import Card from "./src/Components/UI/Card.js";
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -37,15 +38,18 @@ const AddUser = (props) => {
     }
 
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal title="An error occured" message="something went wrong"/>
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     )
 };
 
