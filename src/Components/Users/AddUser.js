b5e2baeb8505211b57574b30813509a0d3866b5e
wisdom-maduchor <maduchorwisdom@gmail.com>
2025-02-03 15:20:43 +0100

    Finalizing project:trigering the close function on the modal

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -9,15 +9,24 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
 
         // validation of form
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: "Invalid input",
+                message: 'please enter a valid username and age (non-empty values)'
+            });
             return;
         }
         if(+enteredAge < 1) {
+            setError({
+                title: 'Invaid Age',
+                message: 'enter a valid age (> 0)'
+            })
             return;
         }
 
@@ -37,9 +46,13 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     }
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return (
         <div>
-            <ErrorModal title="An error occured" message="something went wrong"/>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>
